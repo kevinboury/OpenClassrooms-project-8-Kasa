f6c1d0efa78b56ac9e9071926c7281b512b53414
kevinboury <kevin.boury@gmail.com>
2026-09-11 20:53:34 +0200

    Mise en page AccommodationCard

--- src/components/AccommodationCard/AccommodationCard.tsx
+++ src/components/AccommodationCard/AccommodationCard.tsx
@@ -1,17 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import './AccommodationCard.css'
 
-function AccommodationCard(
-    {
-        title, 
-        id, 
-        cover
-    }: 
-    {
-        title: string, 
-        id: string, 
-        cover: string
-    }) {
+function AccommodationCard( { title, id, cover } : { title: string, id: string, cover: string } ) {
         return (
             <NavLink to={`/accommodation/${id}`} className="accommodation-card">
                 <h2>{title}</h2>
