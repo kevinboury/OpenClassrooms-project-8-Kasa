bdfffc3932a665e35ec3686460ad16cb7c48a282
kevinboury <kevin.boury@gmail.com>
2026-09-15 11:17:41 +0200

    Corrections mineures pour design responsive

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -6,11 +6,19 @@ function About() {
   return (
     <div>
         <Banner page="about" />
-        <div className="about__accordion-container">
-            <Accordion title="Fiabilité" contents={["Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."]} />
-            <Accordion title="Respect" contents={["La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."]} />
-            <Accordion title="Service" contents={["La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."]} />
-            <Accordion title="Sécurité" contents={["La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond au critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des atelirs sur la sécurité domestique pour nos hôtes."]} />
+        <div className="about__accordions-container">
+            <div className="about__accordion-container">
+                <Accordion title="Fiabilité" contents={["Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."]} />
+            </div>
+            <div className="about__accordion-container">
+                <Accordion title="Respect" contents={["La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."]} />
+            </div>
+            <div className="about__accordion-container">
+                <Accordion title="Service" contents={["La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."]} />
+            </div>
+            <div className="about__accordion-container">
+                <Accordion title="Sécurité" contents={["La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond au critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des atelirs sur la sécurité domestique pour nos hôtes."]} />
+            </div>
         </div>
     </div>
   )
